c5794571f37db7bf448a791b7d0b15a56be93368
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-02-28 08:42:39 +0530

    Create createRelease.groovy

---
+++ createRelease.groovy
@@ -0,0 +1,39 @@
+import com.xebialabs.xlrelease.domain.Release
+Release createdRelease = xlr {
+  release('uname') {
+    variables {
+      stringVariable('output') {
+        required false
+        showOnReleaseStart false
+      }
+      stringVariable('err') {
+        required false
+        showOnReleaseStart false
+      }
+    }
+    //scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-14T13:00:00+0000')
+    autoStart(true)
+    scriptUsername 'admin'
+    scriptUserPassword 'Password'
+    phases {
+      phase('New Phase') {
+        tasks {
+          custom('uname') {
+            script {
+              type 'remoteScript.Unix'
+              script 'uname -a'
+              output variable('output')
+              err variable('err')
+              address 'localhost'
+              username '${global.rundeckuser}'
+              privateKeyFile '${global.rundeckkeyfile}'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+releaseVariables["releaseID"] = createdRelease.id
+print "\n This is the Release ID of the newly created Release : - \n"
+print releaseVariables["releaseID"]
