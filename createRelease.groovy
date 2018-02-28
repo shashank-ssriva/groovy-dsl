f5e48b0ff19304fda71ea39924c4c6db92af4f8e
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-02-28 10:27:11 +0530

    Update createRelease.groovy

--- createRelease.groovy
+++ createRelease.groovy
@@ -1,6 +1,6 @@
 import com.xebialabs.xlrelease.domain.Release
 Release createdRelease = xlr {
-  release('Add Watcher') {
+  release('uname') {
     variables {
       stringVariable('output') {
         required false
@@ -11,10 +11,9 @@ Release createdRelease = xlr {
         showOnReleaseStart false
       }
     }
-    //scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-14T13:00:00+0000')
-    autoStart(true)
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-14T13:00:00+0000')
     scriptUsername 'admin'
-    scriptUserPassword 'Password'
+    scriptUserPassword 'password'
     phases {
       phase('New Phase') {
         tasks {
@@ -25,15 +24,9 @@ Release createdRelease = xlr {
               output variable('output')
               err variable('err')
               address 'localhost'
-              username '${global.rundeckuser}'
+              username 'admin'
               privateKeyFile '${global.rundeckkeyfile}'
             }
-          }
-          task('Task A') {
-            description('This is a demo task')
-            watchers('admin')
-            owner('shashank')
-            tags('demo')
           }
         }
       }
@@ -41,5 +34,4 @@ Release createdRelease = xlr {
   }
 }
 releaseVariables["releaseID"] = createdRelease.id
-print "\n This is the Release ID of the newly created Release : - \n"
 print releaseVariables["releaseID"]
